3417f90f66e82ad0ac84fe79594bf9d10ff06d3a
Adarsha J K <adarshjk20@gmail.com>
2025-07-22 23:05:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -30,10 +30,12 @@ boxes.forEach((box) => {
     if (turnO) {
       //playerO
       box.innerText = "O";
+      box.style.color = "red";
       turnO = false;
     } else {
       //playerX
       box.innerText = "X";
+      box.style.color = "blue";
       turnO = true;
     }
     box.disabled = true;
